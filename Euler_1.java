81d50f4bd412d3120b68d34cce5d53d6a223cd68
ecklost <eklostermeyer@gmail.com>
2014-09-25 12:35:04 -0400

    My code for the first Project Euler problem.

---
+++ Euler_1.java
@@ -0,0 +1,24 @@
+
+public class Euler_1 {
+
+	public static void main(String[] args) {
+		// Program One - Multiples of 3/5
+
+		// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+		// Find the sum of all the multiples of 3 or 5 below 1000.
+
+		// A simple program, but there is always a better way to solve this.
+		
+		int sum = 0;
+		int count = 1;
+		for (; count < 1000; count++)
+		{
+			if  (count % 3 == 0 || count % 5 == 0) 
+			{
+				sum += count;
+				System.out.println(sum);
+			}
+		}
+	}
+
+}
