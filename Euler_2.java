1fcbd0542f2069d7ac9e102ff76e9f4415dca7a8
Eric Klostermeyer <ecklost@users.noreply.github.com>
2015-09-22 19:17:25 -0400

    Update Euler_2.java
    Removed unnecessary comments.

--- Euler_2.java
+++ Euler_2.java
@@ -13,21 +13,6 @@ public class Euler_2 {
 		// By considering the terms in the Fibonacci sequence whose values do not exceed four million, 
 		// find the sum of the even-valued terms.
 		
-		// If you are coming from Project Euler, and have not done the answer, please turn around. Try
-		// solving the problem yourself!
-		
-		// =============
-		// Preconditions
-		// =============
-		// 1) The term must be even.
-		// 2) The sequence cannot go over four million.
-		
-		// =====
-		// Goals
-		// =====
-		// 1) Create a standard Fibonacci sequence. 
-		// 2) Then, add the even number terms. 
-		
 		int runningTotal = 0;
 		int lowFibo = 1;
 		int highFibo = 2;
