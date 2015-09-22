f5e5b5808e4efc13ec0b0776aa3bd15b2765d369
Eric Klostermeyer <ecklost@users.noreply.github.com>
2015-09-22 17:39:50 -0400

    Update Euler_9.java
    Removed most of the comments, leaving only the necessaries.

--- Euler_9.java
+++ Euler_9.java
@@ -15,44 +15,23 @@ public class Euler_9 {
 			// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 			// Find the product abc.
 
-		// =============
-		// Preconditions
-		// =============
-		// 1) Three numbers in which a < b < c
-		// 2) a^2 + b^2 = c^2, or the Pythagorean theorem.
-		// 3) a + b + c = 1000
-		
-		// After these three conditions are met, the product can be determined.
-		// Cycling through all the numbers would be a possibility, but might take
-		// a long time to compute. The search can be refined in a number of ways.
-		// For instance, the second and third precondition can have numbers 
-
 		System.out.println("Executing...\n");
-		
-		findSetOfNumbers(); // The only piece of code main implements is this method.
-							// The main method is out of the picture at this point.
-		
+		findSetOfNumbers(); 
 		System.out.println("Ending...");
 	}
 
 	public static boolean isThousand(int a, int b,  int c) 
 	{		
-		// Checks to see if the three numbers add up to 1000.
-		
 		return (a + b + c == 1000);	
 	}
 	
 	public static boolean isPythagorean(int a, int b, int c)
 	{
-		// Checks to see if a^2 + b^2 = c^2
-		
 		return (a*a + b*b == c*c);
 	}
 	
 	public static boolean isLessThan(int a, int b, int c)
 	{
-		// Checks the first precondition: a < b < c
-		
 		return ((a < b) && (b < c));
 	}
 	
@@ -68,11 +47,9 @@ public class Euler_9 {
 							{
 								// System.out.println(a + " " + b + " " + c + "\n");
 								System.out.println("The product is: " + (a*b*c) + "\n");
-								
 							}
 						}
 				}
 		}
-		
 	}
 }
